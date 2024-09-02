a51ee797c4cc32bae98d036b0dc81e13d42dca22
Pranav K <pranavkcse@gmail.com>
2024-09-03 00:33:47 +0530

    patch(frontend): redesigned the admin login and signup pages

--- frontend/src/pages/admin/AdminSignup.jsx
+++ frontend/src/pages/admin/AdminSignup.jsx
@@ -1,19 +1,16 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { adminSignup } from "../../store/admin/adminSlice";
-import Header from "../../components/auth/Header";
+import { toast } from "react-toastify";
 
-export default function AdminSignUp() {
+export default function AdminSignup() {
   const [formData, setFormData] = useState({
     name: "",
+    email: "",
     password: "",
   });
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -36,112 +33,122 @@ export default function AdminSignUp() {
         setError(data.message);
         return;
       }
-      dispatch(adminSignup(data));
-      navigate("/admin");
-    } catch (err) {
-      console.log(err);
+      navigate("/admin/sign-in");
+      toast.success("Signup Successful. Please log in.", {
+        autoClose: 2000,
+        hideProgressBar: true,
+      });
+    } catch (error) {
       setLoading(false);
       setError("An error occurred. Please try again.");
     }
   };
 
   return (
-    <>
-      <Header />
-      <div className="w-full">
-        <div className="flex h-full flex-col justify-center before:min-h-[4rem] before:flex-1 after:flex-1 md:before:min-h-[5rem] mt-0">
-          <div className="px-4 sm:px-6">
-            <div className="mx-auto w-full max-w-sm">
-              <div className="py-16 md:py-20">
-                <div className="max-w-md mx-auto p-6 pt-0">
-                  <div className="mb-10">
-                    <h1 className="text-4xl font-bold">Create Admin Account</h1>
-                  </div>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Create Admin Account
+        </h2>
+      </div>
 
-                  {error && (
-                    <p className="text-red-700 mt-5 mb-5 font-semibold">
-                      {error}
-                    </p>
-                  )}
-
-                  <form onSubmit={handleSubmit}>
-                    <div className="space-y-4">
-                      <div>
-                        <label
-                          className="mb-1 block text-sm font-medium text-gray-700"
-                          htmlFor="name"
-                        >
-                          Admin Name
-                        </label>
-                        <input
-                          id="name"
-                          className="form-input w-full py-2"
-                          type="text"
-                          placeholder="Admin Name"
-                          required
-                          onChange={handleChange}
-                        />
-                      </div>
-                      <div>
-                        <label
-                          className="mb-1 block text-sm font-medium text-gray-700"
-                          htmlFor="password"
-                        >
-                          Password
-                        </label>
-                        <input
-                          id="password"
-                          className="form-input w-full py-2"
-                          type="password"
-                          autoComplete="on"
-                          placeholder="••••••••"
-                          required
-                          onChange={handleChange}
-                        />
-                      </div>
-                    </div>
-                    <div className="mt-6 space-y-3">
-                      <button
-                        className="btn w-full bg-gradient-to-t from-blue-600 to-blue-500 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%]"
-                        disabled={loading}
-                      >
-                        {loading ? "Loading..." : "Register Admin"}
-                      </button>
-                    </div>
-                  </form>
-
-                  <div className="mt-6 text-center">
-                    <p className="text-sm text-gray-500">
-                      By signing up, you agree to the{" "}
-                      <Link
-                        to="#"
-                        className="whitespace-nowrap font-medium text-gray-700 underline hover:no-underline"
-                      >
-                        Terms of Service
-                      </Link>{" "}
-                      and{" "}
-                      <Link
-                        to="#"
-                        className="whitespace-nowrap font-medium text-gray-700 underline hover:no-underline"
-                      >
-                        Privacy Policy
-                      </Link>
-                      .
-                    </p>
-                  </div>
-
-                  <div className="flex gap-2 mt-4 ml-5">
-                    <p>Already have an admin account?</p>
-                    <Link to="/admin">
-                      <span className="text-blue-500">Sign in</span>
-                    </Link>
-                  </div>
-                </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          {error && (
+            <div className="mb-4 text-red-600 text-sm font-medium">{error}</div>
+          )}
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Admin Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  onChange={handleChange}
+                />
               </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                disabled={loading}
+              >
+                {loading ? "Signing up..." : "Sign up"}
+              </button>
+            </div>
+          </form>
+
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300"></div>
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">
+                  Already have an admin account?
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <Link
+                to="/admin"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-gray-50"
+              >
+                Log in
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- frontend/src/pages/admin/Login.jsx
+++ frontend/src/pages/admin/Login.jsx
@@ -1,29 +1,35 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { adminLogin, adminLoginFailure } from "../../store/admin/adminSlice.js";
 import { toast } from "react-toastify";
 
-export default function Login() {
-  const [formData, setFormData] = useState({});
-  const [error, setError] = useState("");
+export default function AdminLogin() {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { admin, isLogged } = useSelector((state) => state.admin);
+  const { isLogged } = useSelector((state) => state.admin);
 
   useEffect(() => {
     if (isLogged) {
       navigate("/admin/home");
     }
-  }, []);
+  }, [isLogged, navigate]);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
-    console.log(formData);
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      setLoading(true);
       const res = await fetch("/api/admin/login", {
         method: "POST",
         headers: {
@@ -32,56 +38,111 @@ export default function Login() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      dispatch(adminLogin(data));
-
-      console.log(data);
-
+      setLoading(false);
       if (data.success === false) {
+        setError(data.message);
         dispatch(adminLoginFailure(data));
-        navigate("/admin");
-        toast.error(data.message, {
-          autoClose: 500,
-          hideProgressBar: true,
-        });
         return;
       }
-      navigate("/admin/home", { replace: true });
-      toast.success("Login Successfull", {
-        autoClose: 500,
-        className: "text-green-600",
+      dispatch(adminLogin(data));
+      navigate("/admin/home");
+      toast.success("Login Successful", {
+        autoClose: 2000,
         hideProgressBar: true,
       });
     } catch (error) {
+      setLoading(false);
+      setError("An error occurred. Please try again.");
       dispatch(adminLoginFailure(error));
     }
   };
+
   return (
-    <div className="p-3 max-w-md mx-auto ">
-      <h1 className="text-3xl text-slate-700 text-center font-semibold my-14 ">
-        Admin Sign In
-      </h1>
-      <p className="text-red-700 mt-5 mb-5 font-semibold ml-3">
-        {error ? error || "Something went wrong" : ""}
-      </p>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
-        <input
-          type="string"
-          placeholder="Name"
-          id="name"
-          className="bg-slate-100 p-3 rounded-lg my-2"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          id="password"
-          className="bg-slate-100 p-3 rounded-lg my-2"
-          onChange={handleChange}
-        />
-        <button className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-85 disabled:opacity-80 max-w-lg p-2  my-2">
-          Sign In
-        </button>
-      </form>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Admin Login
+        </h2>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          {error && (
+            <div className="mb-4 text-red-600 text-sm font-medium">{error}</div>
+          )}
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Admin Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                disabled={loading}
+              >
+                {loading ? "Logging in..." : "Log in"}
+              </button>
+            </div>
+          </form>
+
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300"></div>
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">
+                  Don't have an admin account?
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <Link
+                to="/admin/sign-up"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-gray-50"
+              >
+                Sign up
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
